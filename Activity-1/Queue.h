6e69ce7f638e2f2362088ea91397886674a86ee7
Gnashal <147987928+Gnashal@users.noreply.github.com>
2024-09-03 14:05:51 +0000

    Made the display work with an o(1) solution but the last node wont display huhu

--- Activity-1/Queue.h
+++ Activity-1/Queue.h
@@ -58,6 +58,10 @@ bool dequeue (Queue *q) {
 Student front (Queue q) {
     return q.head->data;
 }
+
+Student next (Queue q) {
+    return q.head->next->data;
+}
 void makeNull(Queue *q) {
     NodePtr temp = NULL;
     while(q->head != NULL && q->tail != NULL) {
@@ -67,11 +71,27 @@ void makeNull(Queue *q) {
     }
 }
 
+// Function to compare the "Stduent" struct with another
+bool compareNames(Name name1, Name name2) {
+    return strcmp(name1.fname, name2.fname) == 0 && strcmp(name1.lname, name2.lname) == 0;
+}
+
+bool compareStudents(Student s1, Student s2) {
+    if (s1.studID != s2.studID) return false;
+    if (!compareNames(s1.studName, s2.studName)) return false;
+    if (s1.sex != s2.sex) return false;
+    if (s1.program != s2.program) return false;
+    return true;
+}
+
 void display(Queue q) {
     printf("\n-------------------------------------------------------------\n");
     printf("| %-10s | %-10s | %-7s | %-6s | %-4s |\n", "First Name", "Last Name", "ID", "Sex", "Program");
     printf("-------------------------------------------------------------\n");
-    while (!isEmpty(q)) {
+
+    int displayCount = 0;
+    Student firstNodeSnapshot = front(q);
+    while (!isEmpty(q) && displayCount < 1) {
         printf("| %-10s | %-10s | %-7d | %-6s | %-4s |\n", 
             q.head->data.studName.fname,
             q.head->data.studName.lname, 
@@ -79,7 +99,12 @@ void display(Queue q) {
             getSexString(q.head->data.sex), 
             getProgramString(q.head->data.program)
         );
+        enqueue(&q, front(q));
         dequeue(&q);
+
+        if (compareStudents(firstNodeSnapshot, next(q))) {
+            displayCount++;
+        }
     }
     printf("-------------------------------------------------------------\n");
 }
