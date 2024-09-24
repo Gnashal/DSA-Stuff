fba103da4793e13b237abf7db500d96fe3a4c757
Gnashal <yousifceballos@gmail.com>
2024-09-24 12:17:39 +0800

    DID LINEAR PROBING

---
+++ DSA-Hash/linear_probing/hash.h
@@ -0,0 +1,101 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct {
+    int studID;
+    char studName[20];
+    char program[20];
+} Student;
+ typedef enum {
+    EMPTY, DELETED, OCCUPIED
+} Flag;
+typedef struct {
+    Student data;
+    Flag flag;
+}Data;
+
+typedef struct {
+    Data* elems;
+    int max;
+    int count;
+}HashTable;
+
+bool isEmpty(HashTable ht, int pos) {
+    return ht.elems[pos].flag == EMPTY;
+}
+bool isFull(HashTable ht, int pos) {
+    while(!isEmpty(ht, pos)) {
+        if (ht.elems[pos].flag == EMPTY || ht.elems[pos].flag == DELETED ) {
+            return false;
+        }
+        pos++;
+    }
+}
+Student createStudents(char* program, char* name, int id) {
+    Student newStud;
+    strcpy(newStud.program, program);
+    strcpy(newStud.studName, name);
+    newStud.studID = id;
+    return newStud;
+}
+
+HashTable createHashTable(int max) {
+    HashTable newHashTable;
+    newHashTable.max = max;
+    newHashTable.count = 0;
+    newHashTable.elems = (Data*)calloc(sizeof(HashTable) , newHashTable.max);
+    for (int i = 0; i < newHashTable.max; ++i) {
+        newHashTable.elems[i].flag = EMPTY;
+    }
+    return newHashTable;
+}
+
+
+int getHash(Student s, int max) {
+    int i = 0;
+    int j = 0;
+    int pos = 0;
+    for (; j < strlen(s.program);++j) {
+        if (i < 3) {
+            pos += s.studName[i];
+            ++i;
+        }
+        pos += s.program[j];
+    }
+
+    return pos % max;
+}
+
+bool insert (HashTable *ht, Student s) {
+    int pos = getHash(s, ht->max);
+    if (ht->count > ht->max * 0.80) {
+        return false;
+    }
+    while (!isEmpty(*ht, pos) )
+    {
+        pos++;
+    }
+        ht->elems[pos].data = s;
+        ht->elems[pos].flag = OCCUPIED;
+        ht->count++;
+}
+
+void visualize(HashTable ht) {
+    for (int i = 0; i < ht.max; ++i) {
+        if (ht.elems[i].flag == OCCUPIED) {
+            printf("%d--[%d]--%15s--%15s\n", i, ht.elems[i].data.studID, ht.elems[i].data.studName, ht.elems[i].data.program);
+        } else {
+            printf("%d--%s\n", i, "EMPTY");
+        }
+    }
+}
+
+
+#endif
+
+// jcarlomaster105@gmail.com
+// 23101479@usc.edu.ph
---
+++ DSA-Hash/linear_probing/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "hash.h"
+
+int main() {
+    HashTable ht = createHashTable(10);
+
+
+    Student students[] = {
+        createStudents("Computer Science", "Alice", 1001),
+        createStudents("Computer Science", "Alisha", 1008),
+        createStudents("Computer Science", "Alivine", 1003),
+        createStudents("Engineering", "David", 1004),
+        createStudents("Biology", "Eva", 1005),
+        createStudents("Chemistry", "Frank", 1006),
+        createStudents("Mathematics", "Grace", 1007),
+        createStudents("Computer Science", "Hannah", 1008),
+        createStudents("Physics", "Ian", 1009),
+        createStudents("Engineering", "Jack", 1010),
+        createStudents("Biology", "Kathy", 1011),
+        createStudents("Chemistry", "Leo", 1012),
+        createStudents("Mathematics", "Mona", 1013),
+        createStudents("Computer Science", "Nina", 1014),
+        createStudents("Physics", "Oscar", 1015),
+        createStudents("Engineering", "Pam", 1016),
+        createStudents("Biology", "Quinn", 1017),
+        createStudents("Chemistry", "Rick", 1018),
+        createStudents("Mathematics", "Sophie", 1019),
+        createStudents("Computer Science", "Tom", 1020),
+        createStudents("Engineering", "Uma", 1021)
+    };
+
+
+    for (int i = 0; i < sizeof(students) / sizeof(students[0]); i++) {
+        insert(&ht, students[i]);
+    }
+
+
+
+    visualize(ht);
+
+    return 0;
+}
