2d8252d4b86b14895041c5dbfc26cca08e46e9da
Gnashal <yousifceballos@gmail.com>
2024-10-19 15:23:57 +0800

    added a heapify and build max_heap function

--- DSA-BinaryHeap/main.c
+++ DSA-BinaryHeap/main.c
@@ -1,14 +1,23 @@
 #include "maxheap.h"
 
-int main () {
+int main()
+{
     BinaryHeap heap = createHeap(21);
-    for (int i = 0; i < 20; ++i) {
+    for (int i = 0; i < 20; ++i)
+    {
         insert(&heap, i + 1);
     }
     display(heap);
     removeInHeap(&heap);
     printf("After removing once: \n");
     display(heap);
+
+    printf("Testing heapify_max: \n");
+    int arr[10] = {1, 56, 69, 40, 12, 422, 23, 2, 90, 71};
+    displayArray(arr, 10);
+    build_max_heap(arr, 10);
+    displayArray(arr, 10);
+
     freeHeap(&heap);
     return 0;
 }
--- DSA-BinaryHeap/maxheap.h
+++ DSA-BinaryHeap/maxheap.h
@@ -4,25 +4,29 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
-typedef struct 
+typedef struct
 {
-    int* data;
+    int *data;
     int max;
     int count;
 } BinaryHeap;
 
-BinaryHeap createHeap(int size) {
+BinaryHeap createHeap(int size)
+{
     BinaryHeap bh;
     bh.max = size;
-    bh.data = (int*)malloc(sizeof(int) * bh.max);
+    bh.data = (int *)malloc(sizeof(int) * bh.max);
     bh.count = 0;
     return bh;
 }
 
-bool insert(BinaryHeap* bh, int data) {
-    if (bh->count >= bh->max) return false;
-    int curr = bh->count,  parent;
-    while (curr > 0 && data > bh->data[(curr - 1) / 2]) {
+bool insert(BinaryHeap *bh, int data)
+{
+    if (bh->count >= bh->max)
+        return false;
+    int curr = bh->count, parent;
+    while (curr > 0 && data > bh->data[(curr - 1) / 2])
+    {
         parent = (curr - 1) / 2;
         bh->data[curr] = bh->data[parent];
         curr = parent;
@@ -31,39 +35,95 @@ bool insert(BinaryHeap* bh, int data) {
     bh->count++;
     return true;
 }
-bool removeInHeap(BinaryHeap* bh) {
-    if (bh->count == 0) return false;
+bool removeInHeap(BinaryHeap *bh)
+{
+    if (bh->count == 0)
+        return false;
     --(bh->count);
-    int index =  0;
+    int index = 0;
     int larger = index;
-    while (index < bh->count) {
+    while (index < bh->count)
+    {
         int left = (2 * index) + 1;
-        if (left > bh->count || left + 1 > bh->count) {
+        if (left > bh->count || left + 1 > bh->count)
+        {
             break;
         }
-        larger = (bh->data[left]>bh->data[left+1])? left:left+1;
+        larger = (bh->data[left] > bh->data[left + 1]) ? left : left + 1;
         bh->data[index] = bh->data[larger];
         index = larger;
     }
     bh->data[index] = bh->data[bh->count];
     return true;
 }
-void display(BinaryHeap bh) {
+void display(BinaryHeap bh)
+{
     printf("{");
-    for (int i = 0; i < bh.count; ++i) {
+    for (int i = 0; i < bh.count; ++i)
+    {
         printf("%d", bh.data[i]);
-        if (i < bh.count - 1) {
+        if (i < bh.count - 1)
+        {
             printf(", ");
         }
     }
     printf("}\n");
 }
-void freeHeap(BinaryHeap* bh) {
+void displayArray(int *arr, int size)
+{
+    printf("{");
+    for (int i = 0; i < size; ++i)
+    {
+        printf("%d", arr[i]);
+        if (i < size - 1)
+        {
+            printf(", ");
+        }
+    }
+    printf("}\n");
+}
+bool heapify_max(int *arr, int size, int index)
+{
+    int left, larger;
+    int root = arr[index];
+    while (true)
+    {
+        left = (index * 2) + 1;
+        if (left >= size)
+        {
+            break;
+        }
+        if (left + 1 < size && arr[left + 1] > arr[left])
+        {
+            larger = left + 1;
+        }
+        else
+        {
+            larger = left;
+        }
+        if (root >= arr[larger])
+        {
+            break;
+        }
+        arr[index] = arr[larger];
+        index = larger;
+    }
+    arr[index] = root;
+    return true;
+}
+bool build_max_heap(int *arr, int size)
+{
+    for (int i = (size / 2) - 1; i >= 0; --i)
+    {
+        heapify_max(arr, size, i);
+    }
+}
+void freeHeap(BinaryHeap *bh)
+{
     free(bh->data);
     bh->data = NULL;
     bh->count = 0;
     bh->max = 0;
 }
-
 
 #endif
