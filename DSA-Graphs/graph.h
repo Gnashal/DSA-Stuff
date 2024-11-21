2768f616f077544dfca5f50fec09c3bcffaa97d1
Gnashal <yousifceballos@gmail.com>
2024-11-21 12:31:45 +0800

    added graph stuff

--- DSA-Graphs/graph.h
+++ DSA-Graphs/graph.h
@@ -7,33 +7,105 @@
 #include <string.h>
 
 
+#define MAX 10
+
+
+
+typedef struct {
+    char fname[20];
+    char lname[20];
+}Name;
+
 typedef struct node {
-    char dest;
+    Name name;
     struct node * next;
 }Node, *NodePtr;
 
 typedef struct {
     int vertices;
     NodePtr * adjNodes;
-}*Graph;
+}Graph;
 
-NodePtr createNode(char dest) {
+Name createName(char* fname, char * lname) {
+    Name newName;
+    strcpy(newName.fname ,fname);
+    strcpy(newName.lname ,lname);
+    return newName;
+}
+
+NodePtr createNode(Name name) {
     NodePtr newNode = (NodePtr)malloc(sizeof(Node));
-    newNode->dest = dest;
+    newNode->name = name;
     newNode->next = NULL;
     return newNode;
 }
 
 
 
-Graph createGraph(int vertices) {
-    Graph newGraph = (Graph)malloc(sizeof(Graph));
-    newGraph->vertices = vertices;
-    newGraph->adjNodes = (NodePtr*)malloc(sizeof(Node) * newGraph->vertices);
-     for (int i = 0; i < vertices; i++) {
-        newGraph->adjNodes[i] = NULL;
+Graph createGraph() {
+    Graph newGraph;
+    newGraph.vertices = 0;
+    newGraph.adjNodes = (NodePtr*)malloc(sizeof(NodePtr) * MAX);
+     for (int i = 0; i < MAX; i++) {
+        newGraph.adjNodes[i] = NULL;
     }
     return newGraph;
+}
+
+unsigned long hash(char* fname, char*lname) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *lname +((*fname))++)) {
+        hash = ((hash << 5) + hash) + c;
+    }
+
+    return (int)hash % MAX;
+}
+
+bool insertToGraph(NodePtr newNode, Graph* gh) {
+    if (!newNode) {
+        return false;
+    }
+    int pos = hash(newNode->name.fname, newNode->name.lname);
+    NodePtr curr = gh->adjNodes[pos];
+    if (!curr) {
+        gh->adjNodes[pos] = newNode;
+    } else {
+        while (curr->next) {
+            curr = curr->next;
+        }
+        curr->next = newNode;
+    }
+    gh->vertices++;
+    return true;
+}
+
+
+void display(Graph gh) {
+    for (int i = 0; i < MAX; ++i) {
+        if (gh.adjNodes[i]) {
+             NodePtr curr = gh.adjNodes[i];
+            while (curr)
+            {
+                Name name = curr->name;
+                printf("%d: %s, %s->",i,name.lname, name.fname);
+                curr = curr->next;
+            }
+            printf("\n");
+        }
+    }
+}
+
+void freeGraph(Graph * gh) {
+    for (int i = 0;  i < gh->vertices; ++i) {
+        NodePtr curr = gh->adjNodes[i];
+        while (curr) {
+            NodePtr temp = curr;
+            curr = curr->next;
+            free(temp);
+        }
+    }
+    free(gh->adjNodes);
 }
 
 #endif
--- DSA-Graphs/main.c
+++ DSA-Graphs/main.c
@@ -1,6 +1,32 @@
 #include "graph.h"
-
+#include <stdio.h>
 
 int main () {
+    Graph graph = createGraph();
 
+    Name name1 = createName("John", "Doe");
+    Name name2 = createName("Jane", "Smith");
+    Name name3 = createName("Alice", "Johnson");
+    Name name4 = createName("Alice", "May");
+
+    NodePtr node1 = createNode(name1);
+    NodePtr node2 = createNode(name2);
+    NodePtr node3 = createNode(name3);
+    NodePtr node4 = createNode(name4);
+
+
+    // Insert nodes into the graph
+    insertToGraph(node1, &graph);
+    insertToGraph(node2, &graph);
+    insertToGraph(node3, &graph);
+    insertToGraph(node4, &graph);
+
+    // Display the graph
+    printf("Graph contents:\n");
+    display(graph);
+
+    // Free the graph's memory
+    freeGraph(&graph);
+
+    return 0;
 }
